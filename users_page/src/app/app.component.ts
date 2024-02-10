2464d260bbd5cd88b4739456f261cb1da940ee57
Bamboo52698 <bamboo52698@gmail.com>
2024-02-10 13:26:40 +0530

    feat : uploading after update and delete

--- users_page/src/app/app.component.ts
+++ users_page/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {DataTableDirective} from 'angular-datatables';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 
 @Component({
@@ -13,11 +13,23 @@ import { Subject } from 'rxjs';
 export class AppComponent implements OnInit{
 
   UsersArray : any[] = [];
-  usArray : any[] = ["nme","hh","uu","uu","uu","rr","oo"];
   isResultLoaded = false;
-  isUpdateFormActive = false;
+
+  UserNameUpdate: any;
+  UserEmailUpdate: any;
+  UserMobileNoUpdate: any;
+  currentUserID : any ;
+
+  idToShow : any;
+  isTextboxVisible: boolean = false;
+
   dtoptions : DataTables.Settings= {} ;
   dtTrigger: Subject<any> = new Subject<any>;
+
+  isEditClicked : boolean = false;
+
+ 
+
 
   @ViewChild(DataTableDirective, {static: false})
 datatableElement: any = DataTableDirective;
@@ -26,6 +38,7 @@ datatableElement: any = DataTableDirective;
   {
     this.getUsers();
   }
+
   ngOnInit(): void {
 
     this.dtoptions={
@@ -48,7 +61,44 @@ datatableElement: any = DataTableDirective;
     });
   }
 
+  showTableData(ID: any): void {
+    this.isEditClicked = !this.isEditClicked;
+    this.idToShow = ID;
+  }
 
+  UpdateRecords(useritem : any)
+  {
+    this.currentUserID = useritem.ID;
+    if(this.UserNameUpdate == null){
+      this.UserNameUpdate = prompt("UserName must not be Empty");
+    }else{
+      alert("Are you want to Update these fields");
+    let bodyData = 
+    {
+      "UserName" : this.UserNameUpdate,
+      "UserEmail" : this.UserEmailUpdate,
+      "UserMobileNo" : this.UserMobileNoUpdate
+    }
+  ;
+    
+    this.http.put("http://localhost:8888/api/user/update"+ "/"+ this.currentUserID,bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("User Details Updated")
+      
+    });}
+  }
+
+
+ 
+  setDelete(useritem: any)
+  {
+    this.http.delete("http://localhost:8888/api/user/delete/"+ useritem.ID).subscribe((resultData: any)=>
+    {
+      alert("Are you want to delete these fields");
+        console.log(resultData);
+    });
+  }
 }
 
 
